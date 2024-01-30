ca440593e2e66267473aa2f4a860f8d5d80e7153
Izaan Shumaiz <118472408+izaan-sh@users.noreply.github.com>
2024-01-30 18:46:38 +0400

    Add files via upload

---
+++ findavg.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+int main (){
+
+float A;
+float B;
+float Average;
+fprintf("Enter the First number");
+fscanf("%f\n", &A);
+
+fprintf("Enter the Second number")
+fscanf("%f\n", &B);
+
+Average = (A + B) / 2;
+
+fprintf("the avg of %f and %f is: %f\n", Average);
+
+fprintf("Print any key to exit the process");
+getchar();
+
+}
